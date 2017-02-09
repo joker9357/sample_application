5032221c0dab931f7cdd782d0f22a5546a6a48e3
joker9357 <jokernjust@gmail.com>
2017-02-09 11:30:09 -0600

    Implement advance login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,5 +1,7 @@
 require 'test_helper'
 
+require 'test_helper'
+
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
   test "invalid signup information" do
@@ -11,19 +13,5 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
-    assert_select 'div#<CSS id for error explanation>'
-    assert_select 'div.<CSS class for field with error>'
-
-    test "valid signup information" do
-      get signup_path
-      assert_difference 'User.count', 1 do
-        post users_path, params: { user: { name:  "Example User",
-                                           email: "user@example.com",
-                                           password:              "password",
-                                           password_confirmation: "password" } }
-      end
-      follow_redirect!
-      assert_template 'users/show'
-    end
   end
 end
